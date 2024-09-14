e9eed1c912e939f0a240e1ee37d10fca170dfa33
Vitor Capdeville <vgcapdeville@hotmail.com>
2024-09-14 11:12:32 -0300

    ✅ Incluir testes para precision e recall.

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,12 @@
 import keras
+import numpy as np
 import pytest
 import tensorflow as tf
+from sklearn.metrics import precision_score, recall_score
 
-THRESHOLD = 0.95
-
+ACCURACY_THRESHOLD = 0.95
+PRECISION_THRESHOLD = 0.5
+RECALL_THRESHOLD = 0.5
 
 @pytest.fixture
 def model():
@@ -15,6 +18,22 @@ def data():
     return tf.data.Dataset.load("tests/ds_test_sample")
 
 
-def test_model(model, data):
+def test_model_accurary(model, data):
     accuracy = model.evaluate(data)[1]
-    assert accuracy >= THRESHOLD
+    assert accuracy >= ACCURACY_THRESHOLD
+
+
+def test_category_precision_and_recall(model, data):
+    predictions = model.predict(data)
+    predicted_labels = np.argmax(predictions, axis=1)
+    true_labels = []
+    for _, label in data:
+        true_labels.append(label.numpy())
+
+    true_labels = np.concatenate(true_labels)
+
+    precision = precision_score(true_labels, predicted_labels, average=None)
+    recall = recall_score(true_labels, predicted_labels, average=None)
+
+    assert (precision >= PRECISION_THRESHOLD).all()
+    assert (recall >= RECALL_THRESHOLD).all()
